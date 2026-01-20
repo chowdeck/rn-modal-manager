db8a4e5c1bc624cba7ac530ff13b694e00634569
sauban <saubanlawal@gmail.com>
2026-01-20 14:56:38 +0100

    Fix peer dependency issue

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export * from './src/AppModal'
-export * from './src/types'
-export * from './src/modalStore'
+export * from './src/AppModal';
+export * from './src/types';
+export * from './src/modalStore';
